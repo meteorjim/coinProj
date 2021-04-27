bccf6b9e390381871fdf5aceec8eff93a477792f
Jim <liujingming@cmiot.chinamobile.com>
2021-04-27 16:58:38 +0800

    udpate

--- test.py
+++ test.py
@@ -1,11 +1,15 @@
 from time import sleep
 from utils import *
+from plotly import graph_objects as go
 
 symbol = "dogeusdt"
-leverage = 50
+leverage = 30
 direction = "buy"
 init_win_rates = 20
-force_flat = -30
+force_close = -20
+change_rate = 10
+
+plot_list = []
 
 #TODO 获取当前持仓信息
 open_point=get_price(symbol, "1min", 1).iloc[-1]["close"]
@@ -17,17 +21,24 @@ while True:
         rates = (current_point - open_point)/open_point * 100 * leverage
     elif direction == "sell":
         rates = (open_point - current_point)/open_point * 100 * leverage
+    else:
+        raise Exception("wha???????")
     if pr != rates:
-        print(f"current rate = {rates}")
+        print("current rate = {:.4f}%".format(rates))
+        plot_list.append(rates)
     pr = rates
-    if rates < force_flat:
+    if rates < force_close:
         if rates < 0:
             print("I quite QAQ")
         else:
-            print(f"I quite XD, and win{abs(current_point-open_point)}, with {rates}%")
+            print(f"I quite XD, with {rates}%")
         break
     
     if rates > init_win_rates:
-        force_flat = init_win_rates - 10
-        init_win_rates += 10
-    # sleep(0.3)
+        force_close = init_win_rates - change_rate
+        init_win_rates += change_rate
+
+fig = go.Figure(
+    go.Scatter(y=plot_list)
+)
+fig.show()
