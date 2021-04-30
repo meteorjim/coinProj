767bc3c92a65b8fdf694539165c1a221247cbaa4
Jim <liujingming@cmiot.chinamobile.com>
2021-04-30 12:28:39 +0800

    update proj

--- usdt_swap.py
+++ usdt_swap.py
@@ -6,6 +6,7 @@ import os
 import sys
 import base64
 from urllib.parse import urlencode
+from configparser import ConfigParser
 
 import requests
 
@@ -13,9 +14,16 @@ if sys.platform == "win32":
     os.environ.update({"HTTP_PROXY": "socks5h://127.0.0.1:12315"})
     os.environ.update({"HTTPS_PROXY": "socks5h://127.0.0.1:12315"})
 
-def send_swap_requests(method: str, url: str, **kwargs) -> dict:
+conf = ConfigParser()
+conf.sections()
+conf.read("keys.conf", "utf8")
+key_conf=conf["key"]
 
-    host = "https://api.hbdm.com"
+host = "https://api.hbdm.com"
+api_key = key_conf["api_key"]
+secret_key = key_conf["secret_key"]
+
+def send_swap_requests(method: str, url: str, **kwargs) -> dict:
 
     query = {
         "AccessKeyId": api_key,
@@ -23,31 +31,29 @@ def send_swap_requests(method: str, url: str, **kwargs) -> dict:
         "SignatureVersion": "2",
         "Timestamp": datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S")
     }
-    if kwargs:
-        query.update(kwargs)
-    query = dict(sorted(query.items()))
-    prepare_sig = method.upper()+"\n"+host[8:]+"\n"+url+"\n"+urlencode(query)
     query = urlencode(query)
+    prepare_sig = method.upper()+"\n"+host[8:]+"\n"+url+"\n"+query
     pre_sig = hmac.new(secret_key.encode("utf8"),
                        prepare_sig.encode("utf8"), 
                        digestmod=hashlib.sha256)
     sig_part = urlencode({"Signature": base64.b64encode(pre_sig.digest()).decode("utf8")})
     query = query+"&"+sig_part
-    resp = requests.request(method, host+url+"?"+query)
+    resp = requests.request(method, host+url+"?"+query, json=kwargs)
     return json.loads(resp.content.decode("utf8"))
 
 def get_current_swap_info(contract_code: str) -> dict:
-    url = "/linear-swap-api/v1/swap_position_info"
     method = "post"
-    d1=datetime.datetime.now()
+    url = "/linear-swap-api/v1/swap_position_info"
     resp = send_swap_requests(method, url, contract_code=contract_code)
-    print(datetime.datetime.now()-d1)
-    return resp["data"][0]
+    if resp.get("data"):
+        return resp["data"][0]
+    else:
+        return None
 
-def fast_close(contract_code: str, volume: float, direction: str):
+def fast_close(contract_code: str, volume: int, direction: str):
     method = "POST"
     url = "/linear-swap-api/v1/swap_lightning_close_position"
-    resp = send_swap_requests(method, url, contract_code=contract_code, volume=volume, direction=direction)
+    resp = send_swap_requests(method, url, contract_code=contract_code, volume=int(volume), direction=direction)
     return resp
 
 def reverse_direction(direction: str):
@@ -57,9 +63,3 @@ def reverse_direction(direction: str):
         return "buy"
     else:
         Exception("wha????")
-
-if __name__ == "__main__":
-    contract_code = "TRX-USDT"
-    result = get_current_swap_info(contract_code)
-    resp = fast_close(contract_code, 2, reverse_direction(result["direction"]))
-    print(resp)
